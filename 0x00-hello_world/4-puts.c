7a164d88320b50e3d6619ba82da1165ed29b4c8a
Un-tony <antonyomondi755@gmail.com>
2023-02-16 11:35:23 -0800

    starting C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * File : 4-puts.c
+ */
+
+#include <stdio.h>
+
+/**
+ * main - '\"Programming is like building
+ * a multilingual puzzle'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
